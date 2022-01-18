10c6ab804632b4943dde719f8b5d2b073f980954
mayank4747 <sugarddy.673@gmail.com>
2022-01-18 17:27:08 +0530

    work

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4def6761e80e1016"; }
+/******/ 		__webpack_require__.h = function() { return "29d9d1e3fa2f6fcf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
